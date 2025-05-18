d620b933f80d6227ab94e1c2580e5f16312cb640
Michael Lindner <mlindner.mail@gmx.de>
2025-05-18 12:59:52 +0200

    1) add new defs prepare_nonlinear and get_nonlinear_status to data.py for the usage of nonlinear JidtGaussianCMI and 2) add test\systemtest_nonlinear_granger for nonlinear JidtGaussianCMI

--- idtxl/data.py
+++ idtxl/data.py
@@ -216,6 +216,129 @@ class Data():
         self.n_samples = data.shape[1]
         self.n_replications = data.shape[2]
 
+    def prepare_nonlinear(self, settings, data):
+        """ prepare data for nonlinear data analysis (nonlin_granger)
+
+        adding squared processes to data and overiting data
+        e.g.
+            original data
+                2 processes, n samples, m replications
+                    target, n, m
+                    source, n, m
+
+            new data:
+                4 processes, n samples, m replications
+                    target, n, m
+                    source, n, m
+                    target^2, n, m
+                    source^2, n, m
+
+        OUTPUT: settings, data
+            add to settings
+                settings["nonlinear_prepared"] = True
+                settings["nonlinear_target"] - original target (int)
+                settings["nonlinear_orig_sources"] - orig_sources (list)
+                settings["nonlinear_all_targets"] - all targets (list)
+                settings["nonlinear_all_sources"] - all sources (list)
+                settings["nonlinear_process_desc"] - (list) n*2 x 2 infos sources vs targets and orig vs squared
+                    e.g. [["target", "source", "target", "source"]
+                          ["orig", "orig", "squared", "squared"]]
+
+            add to data
+                overwrite data
+                new attr. nonlinear_prepared = True
+                new attr. nonlinear_process_desc (list) n*2 x 2 infos sources vs targets and orig vs squared
+                    e.g. [["target", "source", "target", "source"]
+                          ["orig", "orig", "squared", "squared"]]
+
+        Args:
+            data : idtxl Data Object (NOT NORMALIZED!)
+
+            settings : DICT
+                target : int
+                    specifies the target process in the data
+                sources(optional) : int, list of int, or "all". (default: "all")
+                    specifies sources in the data
+
+
+        """
+
+        if data.normalise == True:
+            raise RuntimeError("Data needs to be NOT normalized!")
+
+        # prepare normalising data
+        data.normalise = True
+
+        # add nonlinear processes
+        data.set_data(np.concatenate((data.data, np.square(data.data)), axis=0), "psr")
+
+        # Flag data
+        data.nonlinear_prepared = True
+
+        # define targets
+        if "target" not in settings:
+            raise RuntimeError("Target not specified in settings!")
+        else:
+            targets = [int(settings["target"]), int(settings["target"] + data.n_processes / 2)]
+
+        # create source list depending on input
+        if "sources" not in settings:
+            s = list(range(0, int(data.n_processes)))
+            orig_sources = 'all'
+            # cut targets from source list
+            sources = [j for i, j in enumerate(s) if i not in targets]
+        elif settings["sources"] == "all":
+            s = list(range(0, int(data.n_processes)))
+            orig_sources = 'all'
+            # cut targets from source list
+            sources = [j for i, j in enumerate(s) if i not in targets]
+        else:
+            if type(settings["sources"]) == int:
+                sl = [settings["sources"]]
+            elif type(settings["sources"]) == list:
+                sl = settings["sources"]
+            else:
+                raise RuntimeError("Sources need to be give as int or list of int!")
+
+            sources = sl + [x + int(data.n_processes/2) for x in sl]
+            orig_sources = settings["sources"]
+
+        ## cut targets from source list
+        #sources = [j for i, j in enumerate(s) if i not in targets]
+
+        # create lists for nonlinear processes
+        nonlinear_process_desc = [[0 for i in range(data.n_processes)] for j in range(2)]
+        #for i in range(len(sources)):
+        for i in sources:
+            nonlinear_process_desc[0][i] = "source"
+        for i in range(len(targets)):
+            nonlinear_process_desc[0][targets[i]] = "target"
+        for i in range(int(data.n_processes/2)):
+            nonlinear_process_desc[1][i] = "orig"
+            nonlinear_process_desc[1][i+int(data.n_processes/2)] = "squared"
+
+        data.nonlinear_process_desc = nonlinear_process_desc
+
+        # update settings
+        settings["nonlinear_prepared"] = True
+        settings["nonlinear_target"] = settings["target"]
+        settings["nonlinear_orig_sources"] = orig_sources
+        settings["nonlinear_all_targets"] = targets
+        settings["nonlinear_all_sources"] = sources
+        settings["nonlinear_process_desc"] = nonlinear_process_desc
+
+        return settings, data
+
+    def get_nonlinear_status(self):
+        """return bool if data is prepared for nonlinear_gauss"""
+        if hasattr(self, "nonlinear_prepared"):
+            if self.nonlinear_prepared != True:
+                return False
+            else:
+                return True
+        else:
+            return False
+
     def get_seed(self):
         """return the initial seed of the data"""
         return self.initial_state
@@ -745,6 +868,31 @@ class Data():
                     + term_2 * x[4, n - 1, r]
                     + np.random.normal()
                 )
+        self.set_data(x[:, 3:, :], "psr")
+
+    def generate_nonlinear_data(self, n_samples=1000, n_replications=10):
+        """Generate example data for nonlinear granger testing.
+
+        Generate example data and overwrite the instance's current data.
+
+        The data consists of two auto-regressive (AR) processes with the following coupling:
+
+        0 -> 1, u = 2 (non-linear)
+        """
+        n_processes = 2
+
+        x = np.zeros((n_processes, n_samples + 3, n_replications))
+        x[:, 0:3, :] = np.random.normal(size=(n_processes, 3, n_replications))
+        term_1 = 0.95 * np.sqrt(2)
+        for r in range(n_replications):
+            for n in range(3, n_samples + 3):
+                x[0, n, r] = (
+                    term_1 * x[0, n - 1, r]
+                    - 0.9025 * x[0, n - 2, r]
+                    + np.random.normal()
+                )
+                x[1, n, r] = 0.5 * x[0, n - 2, r] ** 2 + np.random.normal()
+
         self.set_data(x[:, 3:, :], "psr")
 
     def generate_var_data(
---
+++ test/systemtest_nonlinear_granger.py
@@ -0,0 +1,56 @@
+"""Run test on nonlinear data preparation and linear vs nonlinear JidtGaussianCMI estimator
+
+"""
+import os
+import time
+import pickle
+from pathlib import Path
+import copy
+
+from idtxl.multivariate_te import MultivariateTE
+from idtxl.data import Data
+
+start_time = time.time()
+
+data = Data()  # initialise an empty data object
+data.normalise = False
+data.generate_nonlinear_data(n_samples=200, n_replications=10)
+data_nonlin = copy.deepcopy(data)
+
+settings = {
+    "target": 1,
+    "sources": 0,
+    "cmi_estimator": "JidtGaussianCMI",
+    "n_perm_max_stat": 500,
+    "n_perm_min_stat": 200,
+    "n_perm_omnibus": 500,
+    "n_perm_max_seq": 500,
+    "max_lag_sources": 5,
+    "min_lag_sources": 1,
+}
+
+# perform JidtGaussianCMI without nonlinear data
+nonlinear_analysis0 = MultivariateTE()
+results0 = nonlinear_analysis0.analyse_single_target(settings, data,
+                                                     target=settings["target"],
+                                                     sources=settings["sources"])
+
+path = Path(os.path.dirname(__file__)).joinpath("data")
+with open(path.joinpath("test_nonlinear_gauss_0"), "wb") as output_file:
+    pickle.dump(results0, output_file)
+
+# prepare data object for nonlinear analysis
+settings_nonlin, data_nonlin = data.prepare_nonlinear(settings, data_nonlin)
+
+# perform JidtGaussianCMI WITH nonlinear data
+nonlinear_analysis1 = MultivariateTE()
+results1 = nonlinear_analysis1.analyse_single_target(settings_nonlin, data_nonlin,
+                                                     target=settings_nonlin["nonlinear_all_targets"],
+                                                     sources=settings_nonlin["nonlinear_all_sources"])
+
+path = Path(os.path.dirname(__file__)).joinpath("data")
+with open(path.joinpath("test_nonlinear_gauss_1"), "wb") as output_file:
+    pickle.dump(results0, output_file)
+
+runtime = time.time() - start_time
+print("---- {0:.2f} minutes".format(runtime / 60))
