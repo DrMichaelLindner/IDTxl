d7da0328ea2942789175854fc1618b4e57bc18f3
Michael Lindner <mlindner.mail@gmx.de>
2025-05-23 11:11:45 +0200

    prepare systemtest for nonlinear granger network analysis

---
+++ test/systemtest_nonlinear_network.py
@@ -0,0 +1,25 @@
+import pickle
+import time
+from idtxl.multivariate_te import MultivariateTE
+from idtxl.data import Data
+
+start_time = time.time()
+data = Data()  # initialise an empty data object
+data.generate_nonlinear_network_data(n_samples=1000, n_replications=10)
+settings = {
+    "cmi_estimator": "JidtGaussianCMI",
+    "n_perm_max_stat": 500,
+    "n_perm_min_stat": 200,
+    "n_perm_omnibus": 500,
+    "n_perm_max_seq": 500,
+    "max_lag_sources": 5,
+    "min_lag_sources": 1,
+}
+
+network_analysis = MultivariateTE()
+results = network_analysis.analyse_network(settings, data)
+runtime = time.time() - start_time
+print("---- {0} minutes".format(runtime / 60))
+
+# Save results
+pickle.dump(results, open('test_nonlinear_network_results.p', 'wb'))
