b8dfef3930a60050d6f8395de4f381d44187d4f9
Michael Lindner <mlindner.mail@gmx.de>
2025-06-04 14:15:18 +0200

    add systemtest_nonlinear_network_mpi.py

---
+++ test/systemtest_nonlinear_network_mpi.py
@@ -0,0 +1,40 @@
+"""Run test on nonlinear data preparation and nonlinear JidtGaussianCMI estimation in network_analysis
+
+start script using (depending on your installed MPI implementation):
+    mpirun -n 16 python systemtest_nonlinear_network_mpi.py
+    srun -n 16 python systemtest_nonlinear_network_mpi.py
+    mpiexec -n 16 python systemtest_nonlinear_network_mpi.py
+"""
+
+import pickle
+import time
+from idtxl.multivariate_te import MultivariateTE
+from idtxl.data import Data
+
+start_time = time.time()
+data = Data()  # initialise an empty data object
+data.generate_mute_data(n_samples=1000, n_replications=10)
+data.normalise = False
+settings = {
+    "MPI": True,
+    "num_threads": 16,
+    "cmi_estimator": "JidtGaussianCMI",
+    "n_perm_max_stat": 500,
+    "n_perm_min_stat": 200,
+    "n_perm_omnibus": 500,
+    "n_perm_max_seq": 500,
+    "max_lag_sources": 5,
+    "min_lag_sources": 1,
+}
+
+# prepare data object for nonlinear analysis
+settings, data = data.prepare_nonlinear(settings, data)
+
+# perform JidtGaussianCMI WITH nonlinear data
+network_analysis = MultivariateTE()
+results = network_analysis.analyse_network(settings, data)
+runtime = time.time() - start_time
+print("---- {0} minutes".format(runtime / 60))
+
+# Save results
+pickle.dump(results, open('test_nonlinear_network_mpi_results.p', 'wb'))
