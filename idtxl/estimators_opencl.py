c7f8fb9c0c4b4e659a47beff8eaa063d929d8c26
DrMichaelLindner <mlindner.mail@gmx.de>
2026-07-07 12:10:14 +0200

    start GaussianCMI in estimators_opencl.py

--- idtxl/estimators_opencl.py
+++ idtxl/estimators_opencl.py
@@ -148,6 +148,96 @@ class OpenCLKraskov(Estimator):
             return 0.9 * self.devices[self.settings['gpuid']].global_mem_size
 
 
+class OpenCLGaussian(Estimator):
+    """Abstract class for implementation of OpenCL Gaussian-estimators.
+
+    Abstract class for implementation of Python Gaussian-estimators, child
+    classes implement estimators for mutual information (MI), conditional
+    mutual information (CMI), 
+
+    actice information storage (AIS) and
+    transfer entropy (TE) 
+
+    using python Gaussian estimator for continuous data. 
+    
+    Estimators can be used to perform multiple, independent searches in
+    parallel. Each of these parallel searches is called a 'chunk'. To search
+    multiple chunks, provide point sets as 2D arrays, where the first
+    dimension represents samples or points, and the second dimension
+    represents the points' dimensions. Concatenate chunk data in the first
+    dimension and pass the number of chunks to the estimators. Chunks must be
+    of equal size.
+
+    Set common estimation parameters for OpenCL estimators.
+
+    Args:
+        settings : dict [optional]
+            set estimator parameters:
+
+            - gpuid : int [optional] - device ID used for estimation (if more
+              than one device is available on the current platform) (default=0)
+            - padding : bool [optional] - pad data to a length that is a
+              multiple of 1024, workaround for a
+            
+            - normalise : bool [optional] - z-standardise data (default=False)
+            - noise_level : float [optional] - random noise added to the data
+              (default=0)
+            - local_values : bool [optional] - return local MI/TE instead of
+              average MI/TE (default=False)
+
+            
+    """  
+    def __init__(self, settings=None):
+        # Get defaults for estimator settings
+        settings = self._check_settings(settings)
+        self.settings = settings.copy()
+        self.settings.setdefault('gpuid', int(0))
+        self.settings.setdefault('noise_level', np.float32(1e-8))
+        self.settings.setdefault('local_values', False)
+        self.settings.setdefault('padding', True)
+        self.settings.setdefault('verbose', True)
+        self.sizeof_float = int(np.dtype(np.float32).itemsize)
+        self.sizeof_int = int(np.dtype(np.int32).itemsize)
+
+        # Get kernel and devices.
+        self.devices, self.context, self.queue = self._get_device(
+                                                        self.settings['gpuid'])
+
+    def is_parallel(self):
+        return True
+
+    def is_analytic_null_estimator(self):
+        return False
+
+    def _get_device(self, gpuid):
+        """Return GPU devices, context, and queue."""
+        all_platforms = cl.get_platforms()
+        platform = next((p for p in all_platforms if
+                         p.get_devices(device_type=cl.device_type.GPU) != []),
+                        None)
+        if platform is None:
+            raise RuntimeError('No OpenCL GPU device found.')
+        my_gpu_devices = platform.get_devices(device_type=cl.device_type.GPU)
+        context = cl.Context(devices=my_gpu_devices)
+        if gpuid > len(my_gpu_devices)-1:
+            raise RuntimeError(
+                'No device with gpuid {0} (available device IDs: {1}).'.format(
+                    gpuid, np.arange(len(my_gpu_devices))))
+        queue = cl.CommandQueue(context, my_gpu_devices[gpuid])
+        logger.debug("Selected Device: {}".format(my_gpu_devices[gpuid].name))
+        return my_gpu_devices, context, queue
+
+    def _get_max_mem(self):
+        """Return max. GPU main memory available for computation."""
+        if 'max_mem' in self.settings:
+            return self.settings['max_mem']
+        elif 'max_mem_frac' in self.settings:
+            return self.settings['max_mem_frac'] * self.devices[
+                                    self.settings['gpuid']].global_mem_size
+        else:
+            return 0.9 * self.devices[self.settings['gpuid']].global_mem_size
+
+
 class OpenCLKraskovMI(OpenCLKraskov):
     """Calculate mutual information with OpenCL Kraskov implementation.
 
@@ -183,6 +273,7 @@ class OpenCLKraskovMI(OpenCLKraskov):
         # Set default estimator settings.
         super().__init__(settings)
         self.settings.setdefault('lag_mi', 0)
+
 
     def estimate(self, var1, var2, n_chunks=1):
         """Estimate mutual information.
@@ -268,6 +359,7 @@ class OpenCLKraskovMI(OpenCLKraskov):
             return mi_array, distances, count_var1, count_var2
         else:
             return mi_array
+
 
     def _estimate_single_run(self, var1, var2, n_chunks=1):
         """Estimate mutual information in a single GPU run.
@@ -816,3 +908,123 @@ class OpenCLKraskovCMI(OpenCLKraskov):
                     count_cnd[:signallength_orig])
         else:
             return cmi_array
+
+
+
+
+class OpenCLGaussianMI(OpenCLGaussian):
+    """Calculate mutual information with OpenCL Kraskov implementation.
+
+    Calculate the mutual information (MI) between two variables using OpenCL
+    GPU-code. See parent class for references.
+
+    Results are returned in nats.
+
+    Args:
+        settings : dict [optional]
+            set estimator parameters:
+
+            - gpuid : int [optional] - device ID used for estimation (if more
+              than one device is available on the current platform) (default=0)
+            - normalise : bool [optional] - z-standardise data (default=False)
+            - noise_level : float [optional] - random noise added to the data
+              (default=1e-8)
+            - lag_mi : int [optional] - time difference in samples to calculate
+              the lagged MI between processes (default=0)
+    """
+
+    def __init__(self, settings=None):
+        # Set default estimator settings.
+        super().__init__(settings)
+        self.settings.setdefault('lag_mi', 0)
+
+    def estimate(self, var1, var2, n_chunks=1):
+        """Estimate mutual information.
+
+        Args:
+            var1 : numpy array
+                realisations of first variable, either a 2D numpy array where
+                array dimensions represent [(realisations * n_chunks) x
+                variable dimension] or a 1D array representing [realisations],
+                array type should be int32
+            var2 : numpy array
+                realisations of the second variable (similar to var1)
+            n_chunks : int
+                number of data chunks, no. data points has to be the same for
+                each chunk
+
+        Returns:
+            float | numpy array
+                average MI over all samples or local MI for individual
+                samples if 'local_values'=True
+            numpy arrays
+                distances and neighborhood counts for var1 and var2 if
+                debug=True and return_counts=True
+        """
+        # Prepare data: check if variable realisations are passed as 1D or 2D
+        # arrays and have equal no. observations.
+        var1 = self._ensure_two_dim_input(var1)
+        var2 = self._ensure_two_dim_input(var2)
+        assert var1.shape[0] == var2.shape[0]
+        assert var1.shape[0] % n_chunks == 0
+        
+        # Shift variables to calculate a lagged MI.
+        if self.settings['lag_mi'] > 0:
+            var1 = var1[:-self.settings['lag_mi'], :]
+            var2 = var2[self.settings['lag_mi']:, :]
+        
+        self._check_number_of_points(var1.shape[0])
+        
+        signallength = var1.shape[0]
+        chunklength = signallength // n_chunks
+        var1dim = var1.shape[1]
+        var2dim = var2.shape[1]
+        pointdim = var1dim + var2dim
+        kraskov_k = self.settings['kraskov_k']
+
+        mem_data = self.sizeof_float * chunklength * pointdim
+        mem_dist = self.sizeof_float * chunklength * kraskov_k
+        mem_ncnt = 2 * self.sizeof_int * chunklength
+        mem_chunk = mem_data + mem_dist + mem_ncnt
+        max_mem = self._get_max_mem()
+
+        max_chunks_per_run = np.floor(max_mem/mem_chunk).astype(int)
+        chunks_per_run = min(max_chunks_per_run, n_chunks)
+
+        logger.debug(
+            'Memory per chunk: {0:.5f} MB, GPU global memory: {1} MB, chunks '
+            'per run: {2}.'.format(
+                mem_chunk / C, max_mem / C, chunks_per_run))
+        if mem_chunk > max_mem:
+            raise RuntimeError('Size of single chunk exceeds GPU global '
+                               'memory.')
+
+
+        # Calc
+       
+
+
+
+    def _estimate_single_run(self, var1, var2, n_chunks=1):
+        """Estimate mutual information in a single GPU run.
+
+        This method should not be called directly, only inside estimate()
+        after memory bounds have been checked.
+
+        Args:
+            var1 : numpy array
+                realisations of first variable, either a 2D numpy array where
+                array dimensions represent [(realisations * n_chunks) x
+                variable dimension] or a 1D array representing [realisations],
+                array type should be int32
+            var2 : numpy array
+                realisations of the second variable (similar to var1)
+            n_chunks : int
+                number of data chunks, no. data points has to be the same for
+                each chunk
+
+        Returns:
+            float | numpy array
+                average MI over all samples or local MI for individual
+                samples if 'local_values'=True
+        """
