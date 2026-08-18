d6f0710614020a27600f5d377aec69ae107b9050
DrMichaelLindner <mlindner.mail@gmx.de>
2026-08-18 17:55:35 +0200

    remove setup of deleted gaussian_fast_estimation test

--- idtxl/setup_gaussian_fast_estimation.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-package = Extension("gaussian_fast_estimation",
-        ["gaussian_fast_estimation.pyx"], include_dirs=[numpy.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])
-setup(ext_modules=cythonize([package]))
